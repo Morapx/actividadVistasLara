97f706184c05fce3fe84b44561fc51ce8a800515
Morapx <bryan_alexis_1999@hotmail.com>
2021-02-02 10:23:21 -0700

    New

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,15 @@ Route::get('/', function () {
 });
 
 Route::get('notas', function(){
-    return view('notas');
+$notas =[
+        /*'primera nota',
+        'segunda nota',
+        'tercera nota',
+        'cuarrta nota',
+        'quinta nota',*/
+    ];
+
+    return view('notas', ['notas' => $notas]);
 });
 
 route::get('agregar', function(){
