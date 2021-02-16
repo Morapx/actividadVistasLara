bb250f78731cde3e49b0b34b4a0c95ff8cc67463
Morapx <bryan_alexis_1999@hotmail.com>
2021-02-16 10:09:40 -0700

    note

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB; 
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('notas', function(){
-    $notas = [
-        "Primera Nota",
-        "Segunda Nota",
-        "Tercera Nota",
-        "Cuarta Nota",
-    ];
+    $notas = DB::table('notas')->get();
+
+
     return view('notas', ['notas' => $notas]);
 })->name('notas.index');
 
@@ -32,5 +30,9 @@ Route::get('agregar', function(){
 });
 
 Route::get('notas/{id}/editar', function($id){
+    $notas = DB::table('notas')
+        ->where('id', $id)
+        ->first();
+
     return 'Aqui se van a editar las notas' .$id;
 })->name('notas.edit');
